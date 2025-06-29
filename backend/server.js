703ddbec61359964e040befe8803f6b1ca2eb946
MansuriVilla <mansuriadnan2004@gmail.com>
2025-06-29 12:18:17 +0530

    URL changed

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 // Configure CORS for local Vite frontend
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://btntnew.vercel.app/",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
